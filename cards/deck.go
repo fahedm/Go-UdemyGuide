06afce2a712bfbacae7647940e963cb3ac12a1b8
fahedm <fahedmohd8750@gmail.com>
2025-03-12 17:55:53 +0530

    random number with new seed

--- cards/deck.go
+++ cards/deck.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
-	"math/rand"
+	"time"
 )
 
 // create a new type of 'deck'
@@ -55,8 +56,10 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
 	for i := range d{
-		newPosition := rand.Intn(len(d)-1)
+		newPosition := r.Intn(len(d)-1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
