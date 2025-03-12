fa1fd375a5c6d4d02dc1a6cdb6ff76581c9292a2
fahedm <fahedmohd8750@gmail.com>
2025-03-12 17:27:47 +0530

    Write in file, byte conversion

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // create a new type of 'deck'
 // which is a slice of strings
@@ -12,19 +16,28 @@ func newDeck() deck {
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for _, suit := range cardSuits{
-		for _, value := range cardValues{
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
 }
-func (d deck) print(){
-	for i, card := range d{
+func (d deck) print() {
+	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
-func deal(d deck, handSize int) (deck,deck) {
+func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+
 }
--- cards/main.go
+++ cards/main.go
@@ -1,8 +1,8 @@
 package main
 
+// import "fmt"
+
 func main(){
 	cards := newDeck()
-	hand, remaingCards := deal(cards, 5)
-	hand.print()
-	remaingCards.print()
+	cards.saveToFile("my_cards")
 }
