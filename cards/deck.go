5b2ed4cf68eaed0e815911decbaaea5bb9c4226f
fahedm <fahedmohd8750@gmail.com>
2025-03-12 17:40:37 +0530

    reading a file

--- cards/deck.go
+++ cards/deck.go
@@ -41,3 +41,14 @@ func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 
 }
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
