feaea97ab21210e368e965603e475ce2513a0f11
fahedm <fahedmohd8750@gmail.com>
2025-03-22 07:49:06 +0530

    maps

--- map/main.go
+++ map/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":"#ff0000",
-		"green":"#000000",
-	}
+	colors := make(map[string]string)
+	// colors := map[string]string{
+	// 	"red":"#ff0000",
+	// 	"green":"#000000",
+	// }
+
+	colors["white"] = "#ffffff"
 	fmt.Println(colors)
 }
